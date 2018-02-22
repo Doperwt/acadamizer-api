6bbbaf20c2379d1c98a18c46364646386f08a10a
doperwt <r.f.denouden@gmail.com>
2018-02-22 19:10:29 +0100

    working on adding reviews

--- routes/classes/students.js
+++ routes/classes/students.js
@@ -2,8 +2,9 @@
 const router = require('express').Router()
 const passport = require('../../config/auth')
 const { Class, User } = require('../../models')
-
 const authenticate = passport.authorize('jwt', { session: false })
+const processUpdate = require('../../lib/processUpdate.js')
+
 
 const loadClass = (req, res, next) => {
   const id = req.params.id
@@ -71,7 +72,39 @@ module.exports = io => {
       })
       res.json(req.students)
     })
+    .patch('/classes/:id/students/:studentId', authenticate, (req, res, next) => {
+      const id = req.params.id
+      const studentId = req.params.studentId
+      console.log(req.params)
+      Class.findById(id)
+        .then((group) => {
+          if (!group) { return next() }
+          // debugger
+          const {review,date,description} = req.body
+          const update = {review:review,date:date,description:description}
+          // const updatedClass = processReview(group, req.body, studentId)
 
+          const student = group.students.filter(s => s._id.toString() === studentId)[0]
+          // const exists = student.reviews.filter(r => r.date === update.date)[0]
+          // if(!!exists) {
+          //   student.reviews = [student.reviews.filter(r => r.date !== update.date ),update]
+          // } else if(student.reviews.length === 0 ) {
+          //   student.reviews = [update]
+          // } else { student.reviews = [student.reviews, update] }
+          // student.lastReview = update.review
+          const updatedClass = processUpdate(group,update,studentId)
+          Class.findByIdAndUpdate(id, { $set: updatedClass }, { new: true })
+            .then((group) => {
+              io.emit('action', {
+                type: 'CLASS_UPDATED',
+                payload: group
+              })
+              res.json(group)
+            })
+            .catch((error) => next(error))
+        })
+        .catch((error) => next(error))
+    })
     .delete('/classes/:id/students', authenticate,loadClass, (req, res, next) => {
       if (!req.group) { return next() }
       console.log(req.group)
